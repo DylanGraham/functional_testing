065e16251177a9a14ecca07e258cca5e0c6cce42
Dylan Graham <dylan.graham@gmail.com>
2017-01-14 21:45:32 +1100

    funks

--- func1.py
+++ func1.py
@@ -1,4 +1,14 @@
 def is_even(x):
     return (x % 2) == 0
 
-print(list(filter(is_even, range(10))))
+
+def is_odd(x):
+    return (x % 2) != 0
+
+
+def provider(func, x):
+    return func(x)
+
+for i in range(10):
+    print(f"Is {i} odd? ", provider(is_odd, i))
+    print(f"Is {i} even? ", provider(is_even, i))
